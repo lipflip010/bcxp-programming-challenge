6be3e3e5cf51c25090d6cd7bee87c3ff3ef52a5d
Philipp Albers <lipflip@posteo.de>
2022-10-31 14:48:10 +0100

    test: add test case for InvalidDataSourceException

--- src/test/java/de/bcxp/challenge/weather/WeatherDataSourceCsvImplTest.java
+++ src/test/java/de/bcxp/challenge/weather/WeatherDataSourceCsvImplTest.java
@@ -2,9 +2,11 @@ package de.bcxp.challenge.weather;
 
 import de.bcxp.challenge.InvalidDataSourceException;
 import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 
@@ -12,16 +14,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 class WeatherDataSourceCsvImplTest {
-    private WeatherDataSourceCsvImpl sut;
 
-    @BeforeEach
-    void setup() {
-        sut = new WeatherDataSourceCsvImpl("src/test/resources/weatherTest.csv");
-    }
 
     @Test
     @DisplayName("should read csv and map data to beans")
     void getDayMeasurements() throws InvalidDataSourceException {
+        WeatherDataSourceCsvImpl sut = new WeatherDataSourceCsvImpl("src/test/resources/weatherTest.csv");
         List<DayMeasurement> result = sut.getDayMeasurements();
 
         DayMeasurement dayMeasurement1 = DayMeasurement.builder().dayOfTheMonth(1).maximumTemperature(40).minimumTemperature(20).build();
@@ -41,5 +39,15 @@ class WeatherDataSourceCsvImplTest {
                 .contains(dayMeasurement2);
 
         assertThat(result).hasSameSizeAs(expected);
+    }
+
+    @Test
+    @DisplayName("should throw exception if file not found")
+    void getDayMeasurementsThrows() {
+        String filePath = "src/test/resources/notfound.csv";
+        WeatherDataSourceCsvImpl sut = new WeatherDataSourceCsvImpl(filePath);
+        Exception exception = assertThrows(InvalidDataSourceException.class, sut::getDayMeasurements);
+
+        assertEquals(String.format("Error: File %s not found. %n", filePath), exception.getMessage());
     }
 }
